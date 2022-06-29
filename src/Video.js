0cc56e3cd8883c5f27d9b84af67df00a97e9a252
Ashish Kashyap <akk312000@gmail.com>
2022-06-29 15:33:09 +0530

    added video

--- src/Video.js
+++ src/Video.js
@@ -1,10 +1,19 @@
 import React, { useRef, useState } from 'react'
+import { BsPlayCircle } from 'react-icons/bs';
 import "./Video.css"
+import Image from './public/dress.png'
 import VideoFooter from './VideoFooter';
 function Video() {
-
 	const [play, setPlay] = useState(false);
 	const videoRef = useRef(null);
+	const isVideoPlaying = videoRef => !!(videoRef.currentTime > 0 && !videoRef.paused && !videoRef.ended && videoRef.readyState > 2);
+	const [showButton, setShowButton] = useState(true)
+		const onButtonClick = () => {
+			setShowButton(!showButton)
+				videoRef.current.play();
+				setPlay(true);
+		}
+
 	const onVideoPress = () => {
 		if (play) {
 			videoRef.current.pause();
@@ -17,15 +26,46 @@ function Video() {
 }
   return (
 	  <div className='video'>
+		  {/* <div>
+			  <BsPlayCircle/>
+		</div> */}
 		  <video className="video__player"
 			  loop
+			  controls
+			//   preload="metadata"
+			  poster={Image}
 			  onClick={onVideoPress}
 			  ref={videoRef}
-			//   controls
-			  src="https://assets.mixkit.co/videos/preview/mixkit-animation-of-futuristic-devices-99786-large.mp4"></video>
-	   <VideoFooter/>
+			  src="https://assets.mixkit.co/videos/preview/mixkit-animation-of-futuristic-devices-99786-large.mp4">
+
+		  </video>
+		
+			  <div className="play-button-wrapper"
+				  onClick={onButtonClick}
+			  >
+				  <div title="Play video" className="play-gif" id="circle-play-b">
+				  {showButton &&
+				  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80">
+						  <path d="M40 0a40 40 0 1040 40A40 40 0 0040 0zM26 61.56V18.44L64 40z" />
+					  </svg>
+				} 
+				  </div>
+			  </div>
+			  <div className="grad">
+{/* <BsPlayCircle/> */}
+			  </div>
+		  {play &&
+				  
+			  <VideoFooter />
+		  }
 	  {/* <VideoSidebar/> */}
-	  
+
+		  {!play &&
+			  <div>
+				  <h2 className='heading'>
+					  Shop My Closet
+			  </h2>
+			  </div>}
 	  </div>
 
   )
--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import './VideoFooter.css';
-function VideoFooter() {
+function VideoFooter({name,description,cost_price,original_price}) {
   return (
 	  <div className='videoFooter'>
 		  {/* <h2>I am a footer</h2> */}
-		  <div className="videoFooter__text">
-			 <h3>Dress Name</h3> 
-			 <p>Dress Description</p>
-		  </div>
 		  <img className="videoFooter__record" src="https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12595594/2020/10/15/3fb18e5c-56b2-46f9-8dde-eb8c6dd689481602737925787SareemallNavyBluePolyChiffonSolidEthnicPartywearSareewithMat2.jpg" alt=""/>
+		  <div className="videoFooter__text">
+			 <h3 className='dressName'>Dress Name</h3> 
+			 <p className='description'>Dress Description</p>
+			 <p className='price'>₹1600</p>
+
+			 <p className='original_price'>₹2000</p>
+			 <p className='discount'>(20% OFF)</p>
+			 <h3 className='shop_button'>SHOP</h3>
+		  </div>
 	</div>
   )
 }
